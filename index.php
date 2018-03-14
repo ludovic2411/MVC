b5506d2f6ce67106b7d4dd77304dd83c2d689a9f
ludovic2411 <ludovic.ceccotti@gmail.com>
2018-03-14 14:29:12 +0100

    before_css

--- index.php
+++ index.php
@@ -5,28 +5,30 @@
  <html>
    <head>
      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
      <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
      <title>Antiquité, Décorations, Belgique-meubles industriels</title>
    </head>
    <body>
      <header>
        <section class="card">
-         <h2 class="Didier">Didier</h2>
-         <h2 class="Motte">Motte</h2>
+         <section class="logo">
+           <a href="index.php"><img src="/MVC/assets/img/touchwood-logo-sans-slogan-nobelBold.png" alt=""></a>
+         </section>
          <p class="presentation"></i>Antiquités .Décoration .Aménagement d'intérieur</p>
          <p class="phone-number"> <i class="fas fa-phone"><a href="+32 495/20.59.34"></a>+32 495/20.59.34</i></p>
          <nav class="nav">
            <ul>
              <li><button type="button" name="button-presentation"> <a href="presentation.php">Présentation</a></button></li>
              <li><button type="button" name="button-stock"><a href="stock.php">Le stock</a></button></li>
-             <li> <button type="button" name="button-contact"><a href="contact.php">Contact</a></button></li>
+             <li><button type="button" name="button-contact"><a href="contact.php">Contact</a></button></li>
            </ul>
          </nav>
        </section>
      </header>
      <?php
      //afficher le stock
-      //include '/views/stock.php';
+      include 'views/stocks/stock-page1.php';
       ?>
       <section class="Quotes">
         <p class="quote">"L'ennui naquit un jour de l'uniformité"</p>
