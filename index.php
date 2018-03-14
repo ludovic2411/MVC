e506ae32ada4e9371f1c6c3a21e62d2f7538e35c
ludovic2411 <ludovic.ceccotti@gmail.com>
2018-03-14 16:52:30 +0100

    mercredi

--- index.php
+++ index.php
@@ -5,25 +5,28 @@
  <html>
    <head>
      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+     <link rel="stylesheet" href="/MVC/assets/css/MVC-style.css">
+     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
      <title>Antiquité, Décorations, Belgique-meubles industriels</title>
    </head>
    <body>
-     <header>
+     <nav class="language"></nav>
+     <header class="main-header">
        <section class="card">
          <section class="logo">
            <a href="index.php"><img src="/MVC/assets/img/touchwood-logo-sans-slogan-nobelBold.png" alt=""></a>
          </section>
-         <p class="presentation"></i>Antiquités .Décoration .Aménagement d'intérieur</p>
-         <p class="phone-number"> <i class="fas fa-phone"><a href="+32 495/20.59.34"></a>+32 495/20.59.34</i></p>
-         <nav class="nav">
+         <p class="presentation">Antiquités .Décoration .Aménagement d'intérieur</p>
+         <p class="phone-number"> <i class="fas fa-phone"><a class="phone-number" href="+32 495/20.59.34">+32 495/20.59.34</a></i></p>
+         <section class="nav">
            <ul>
              <li><button type="button" name="button-presentation"> <a href="presentation.php">Présentation</a></button></li>
              <li><button type="button" name="button-stock"><a href="stock.php">Le stock</a></button></li>
              <li><button type="button" name="button-contact"><a href="contact.php">Contact</a></button></li>
            </ul>
-         </nav>
+         </section>
        </section>
      </header>
      <?php
